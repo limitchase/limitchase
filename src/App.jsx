21a49c4fc4aed77115ac32fe9ce32fe1d11cfcb5
Chase Lim <limitchase@gmail.com>
2023-11-08 22:07:39 -0800

    day 8

--- src/App.jsx
+++ src/App.jsx
@@ -3,27 +3,29 @@ import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
 import Log from "./pages/Log";
 import Home from "./pages/Home";
 import PizzaMenu from "./pages/PizzaMenu";
+import Steps from "./pages/Steps";
 
 const App = () => {
   return (
     <BrowserRouter>
       <nav>
         <p>
+          <Link to="/">Home</Link>
+        </p>
+        <p>
           <Link to="/log">Change Log</Link>
         </p>
         <p>
-          <Link to="me">My Profile</Link>
+          <Link to="/steps">Steps</Link>
         </p>
         <p>
-          <Link to="pizza-menu">Pizza Menu</Link>
-        </p>
-        <p>
-          <Link to="/">Home</Link>
+          <Link to="/pizza-menu">Pizza Menu</Link>
         </p>
       </nav>
       <Routes>
         <Route path="log" element={<Log />} />
         <Route path="pizza-menu" element={<PizzaMenu />} />
+        <Route path="steps" element={<Steps />} />
         {/* <Route path="users/*" element={<Users />} /> */}
         <Route path="/" element={<Home />} />
       </Routes>
